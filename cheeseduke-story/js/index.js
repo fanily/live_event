6a84e86745af55b7502b81053d6157a03d858e85
dylandy.chang <dylandy.chang@gmail.com>
2015-11-23 14:33:09 +0800

    improve ui for pad within landscape

--- cheeseduke-story/js/index.js
+++ cheeseduke-story/js/index.js
@@ -2,5 +2,8 @@ jQuery(function($){
    	$('#clock').countdown('2015/11/27 12:30', function(event) {
  		$(this).html(event.strftime('%D天 %H:%M:%S'));
  	});
+  if(window.matchMedia("screen and (min-device-width: 780px)").matches){
+    $(".container").height($(window).height()-63);    
+  }
 });
 
