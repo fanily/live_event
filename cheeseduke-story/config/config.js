8ca693e84a3eded7005d11341a70616c11dced06
dylandy.chang <dylandy.chang@gmail.com>
2015-11-24 14:49:25 +0800

    changed start comment time from 1230 to 1200

--- cheeseduke-story/config/config.js
+++ cheeseduke-story/config/config.js
@@ -1,7 +1,7 @@
 var target_url = "https://denny.fanily.tw";
 var post_id = "11e58e954b72daf4ac5d42010af020f7";
 var config ={
-  "start_time" : "2015/11/27 12:30",
+  "start_time" : "2015/11/27 12:00",
   "get_comment_url" : target_url + "/post/" + post_id + "/comment?offset=",
   "init_commet_url" : target_url + "/post/" + post_id + "/comment?offset=0",
   "login_url" : target_url + "/auth/login",
