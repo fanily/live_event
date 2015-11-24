f46bb3b6c80f14023ef3449d3b49736dff1b9bbf
dylandy.chang <dylandy.chang@gmail.com>
2015-11-24 14:49:25 +0800

    changed start comment time from 1230 to 1200

--- cheeseduke-story/js/index.js
+++ cheeseduke-story/js/index.js
@@ -1,5 +1,5 @@
 jQuery(function($){
-   	$('#clock').countdown('2015/11/27 12:30', function(event) {
+   	$('#clock').countdown('2015/11/27 12:00', function(event) {
  		$(this).html(event.strftime('%D天 %H:%M:%S'));
  	});
   if(window.matchMedia("screen and (min-device-width: 780px)").matches){
