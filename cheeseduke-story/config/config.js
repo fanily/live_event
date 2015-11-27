e6007efc62b5d35ad75d42fdc83409a72dacee6a
JoeAngel <joe10330@gmail.com>
2015-11-27 11:33:06 +0800

    Add config for online

---
+++ cheeseduke-story/config/config.js
@@ -0,0 +1,11 @@
+var target_url = "https://www.fanily.tw";
+var post_id = "11e5942c16b4ccb19e1542010af01a54";
+var config ={
+  "start_time" : "2015/11/27 12:00",
+  "get_comment_url" : target_url + "/post/" + post_id + "/comment?offset=",
+  "init_commet_url" : target_url + "/post/" + post_id + "/comment?offset=0",
+  "login_url" : target_url + "/auth/login",
+  "send_url" : target_url + "/post/" + post_id + "/comment",
+  "status_url" : target_url + "/auth/loginStatus",
+  "fb_login_url" : target_url + "/auth/fblogin?redirect_url=http://live.fanily.tw/cheeseduke-story/comment-web.html"
+}
