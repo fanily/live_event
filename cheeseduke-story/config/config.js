d98bb3cfc30f60a5d40669bd7d36679f934aa313
dylandy.chang <dylandy.chang@gmail.com>
2015-11-27 17:25:55 +0800

    update config

--- cheeseduke-story/config/config.js
+++ cheeseduke-story/config/config.js
@@ -3,7 +3,7 @@ var post_id = "11e5942c16b4ccb19e1542010af01a54";
 var config ={
   "start_time" : "2015/11/27 12:00",
   "get_comment_url" : target_url + "/post/" + post_id + "/comment?offset=",
-  "init_commet_url" : target_url + "/post/" + post_id + "/comment?offset=0",
+  "init_commet_url" : target_url + "/post/" + post_id + "/comment?offset=0&limit=2000",
   "login_url" : target_url + "/auth/login",
   "send_url" : target_url + "/post/" + post_id + "/comment",
   "status_url" : target_url + "/auth/loginStatus",
