3247840b1ba631b013fcce32dbe1916d426e7cd3
Left Lin <left@left.tw>
2015-12-15 00:57:34 +0800

    update config.js

---
+++ atouchofgreen-live/151216/js/config.js
@@ -0,0 +1,33 @@
+var config = {
+	"start_time" : "2015-12-16 14:00:00",
+	"end_time" : "2015-12-16 16:00:00",
+	"fb_app_id" : "626114784069979",
+	"video_id" : "odTBgVKdTto",
+	"video_start_time" : "",
+	"video_playlist" : "",
+	"title" : "《一把青》全球首航特映會｜Fanily粉絲玩樂獨家直播",
+	"description" : "2015/12/16(三)全球首航特映會，Fanily帶你第一手掌握《一把青》！鎖定直播有獎徵答送《一把青》限量好禮喔！",
+	"keywords" : "一把青,曹瑞原,首映會,公視,Fanily,Fanily粉絲玩樂,空軍,戰爭,藝碩文創,台北創造,結果娛樂,白先勇,台北人,黃世鳴,董陽孜,方序中,陳小霞,吳慷仁,楊一展,楊謹華,天心,藍鈞天,連俞涵,鍾承翰,溫貞菱,直播,朱青,郭軫,師娘,江偉成,秦芊儀,周瑋訓,小周,墨婷,班鐵翔,樊光耀,莊心瑜,楊小黎,華研唱片,田馥甄,林宥嘉,任家萱,Hebe,Selina,周蕙,東山一把青,何燕玲",
+	"og_image" : "http://live.fanily.tw/atouchofgreen-live/151216/images/og_bg.jpg",
+	"og_url" : "http://live.fanily.tw/atouchofgreen-live/151216",
+	"info" : {
+		"title" : "《一把青》<br>全球首航特映會",
+		"content" : "13：30 - 13：50<br>演員後台獨家直擊<br>14：00 首映會正式開始<br>參加直播有獎徵答，<br>送限量《一把青》週邊好禮。",
+		"date" : "2015-12-16（三）<br>13:30",
+		"social" : {
+			"facebook" : "",
+			"google_plus" : "",
+			"twitter" : "",
+			"weibo" : "",
+			"fanily" : "https://www.fanily.tw/group/aTouchofGreen#post",
+		}
+	},
+	"banner" : {
+		"link" : "http://atouchofgreen.fanily.tw/",
+		"image" : "http://live.fanily.tw/atouchofgreen-live/151216/images/banner_600x100.jpg"
+	},
+	"about" : {
+		"title" : "Fanily",
+		"content" : '社群不僅讓你發聲，更讓你「影響」發生！<br><br>Fanily=Fan+family，集結粉絲聚落、娛樂策展、娛樂公投、數據調查，立志成為由粉絲主動發聲的社群娛樂平台。在千千萬萬數位資訊爆炸的世界中，人們需要的不只是接收！Fanily 以溫馴、群居的「馴鹿」作為代表動物，無論你是潛伏網路的「路」人，還是有話想說的「鹿(路)」人，都歡迎加入 Fanily，跟我們一起為台灣找路。',
+	}
+}
