4febe27a26aed4f8b9608ac9833b9637aec1f777
Left Lin <left@left.tw>
2015-12-17 20:05:57 +0800

    add 151219 live

---
+++ atouchofgreen-live/151219/config/modified.js
@@ -0,0 +1,7 @@
+var config = {
+    "live_start_at" : "2015/12/19 14:00",
+    "clock_change_at" : "2015/12/19 13:50",
+    "online_url" : "",
+    "promo_url" : "TxyDnxcnpmg",
+    "time_format" : "%D-%H:%M:%S",
+}
