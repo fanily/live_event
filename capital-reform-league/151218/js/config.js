8d14c45d57c258600721797085a0b88795b6599b
Left Lin <left@left.tw>
2015-12-19 13:08:55 +0800

    getComment form comment.json, remove socket, remove emoji css js

--- capital-reform-league/151218/js/config.js
+++ capital-reform-league/151218/js/config.js
@@ -1,1 +1,41 @@
-var config={start_time:"2015-12-18 21:30:00",end_time:"2015-12-18 23:30:00",close_time:"2015-12-19 01:00:00",fb_app_id:"626114784069979",live:{id:"rHe2xFF5y2s",start_time:"600",playlist:""},api:"https://live-api.fanily.tw",api_socket:"http://live-api.fanily.tw",moderator_uid:["100010460599209","160198881005464"],title:"深夜柯Ｐ 夜問林昶佐",description:"",info:{title:"深夜柯Ｐ 夜問林昶佐",content:"市長柯文哲 晚上變公民<br>鄉民網友一起來<br>一個問十個<br>22:00 夜問林昶佐",date:"2015-12-18（五）<br>22：00",social:{facebook:[{title:"首都改革陣線",link:"https://www.facebook.com/capital.reform.league/?fref=ts"},{title:"林昶佐 粉絲專頁",link:"https://www.facebook.com/ivotefreddy/?fref=nf"}],google_plus:"",twitter:"",weibo:"",fanily:""}},article:{title:"新國會能否符合人民期待，成為改革型進步國會？柯文哲夜問新國會第二場，由第三勢力新興小黨時代力量發起人、萬華/中正區立委候選人林昶佐登場接受柯 P 提問。",content:"重金屬樂團主唱出身，一直以音樂關心政治，並致力推動人權運動，林昶佐決定從體制外圍走向體制核心，要讓『新興力量擁有最大的改革空間』。１２/１8日【五】晚上１０：００準時線上直播民主對談，看林昶佐如何讓柯 P 點頭支持。"}};
+var config = {
+	start_time : "2015-12-18 21:30:00",
+	end_time : "2015-12-18 23:30:00",
+	close_time : "2015-12-19 01:00:00",
+	fb_app_id : "626114784069979",
+	live : {
+		id : "rHe2xFF5y2s",
+		start_time : "600",
+		playlist : "",
+	},
+	api : "http://live-api.ddmay.com",
+	api_socket : "http://live-api.ddmay.com",
+	moderator_uid : ["100010460599209","160198881005464"],
+	title : "深夜柯Ｐ 夜問林昶佐",
+	description : "",
+	info : {
+		title : "深夜柯Ｐ 夜問林昶佐",
+		content : "市長柯文哲 晚上變公民<br>鄉民網友一起來<br>一個問十個<br>22:00 夜問林昶佐",
+		date : "2015-12-18（五）<br>22：00",
+		social : {
+			facebook : [
+				{
+					title: "首都改革陣線",
+					link : "https://www.facebook.com/capital.reform.league/?fref=ts"
+				},
+				{
+					title : "林昶佐 粉絲專頁",
+					link : "https://www.facebook.com/ivotefreddy/?fref=nf"
+				}
+			],
+			google_plus : "",
+			twitter : "",
+			weibo : "",
+			fanily : "",
+		}
+	},
+	article : {
+		title : '新國會能否符合人民期待，成為改革型進步國會？柯文哲夜問新國會第二場，由第三勢力新興小黨時代力量發起人、萬華/中正區立委候選人林昶佐登場接受柯 P 提問。',
+		content : '重金屬樂團主唱出身，一直以音樂關心政治，並致力推動人權運動，林昶佐決定從體制外圍走向體制核心，要讓『新興力量擁有最大的改革空間』。１２/１8日【五】晚上１０：００準時線上直播民主對談，看林昶佐如何讓柯 P 點頭支持。'
+	}
+}
