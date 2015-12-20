e646f3348337a85512a7992f2d988e5371ebc7da
Denny Huang <denny0223@gmail.com>
2015-12-20 14:03:14 +0800

    Update 1520 config

--- atouchofgreen-live/151220/config/modified.js
+++ atouchofgreen-live/151220/config/modified.js
@@ -1,7 +1,7 @@
 var config = {
     "live_start_at" : "2015/12/20 14:00",
     "clock_change_at" : "2015/12/20 13:50",
-    "online_url" : "",
-    "promo_url" : "TxyDnxcnpmg",
+    "online_url" : "QxksC_5LEmM",
+    "promo_url" : "QxksC_5LEmM",
     "time_format" : "%D-%H:%M:%S",
 }
