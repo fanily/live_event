f9a245e41df1b68827fa158a057fcd3cc0c5f12e
JoeAngel <joe10330@gmail.com>
2016-06-08 16:50:40 +0800

    Add yueyuebookstore-160610

---
+++ yueyuebookstore/160610/js/config.js
@@ -0,0 +1,60 @@
+var config = {
+	start_time : "2016-06-10 19:25:00",
+	end_time : "2016-06-10 21:00:00",
+	close_time : "2016-06-10 21:30:00",
+	fb_app_id : "626114784069979",
+	live : {
+		id : "PEHsWsE-ca8",
+		start_time : "",
+		playlist : "",
+	},
+	api : "https://live-api.fanily.tw",
+	api_socket : "http://live-api.fanily.tw",
+	moderator_uid : ["195841904090221","543628245","1714281551","100011616072642","126943094369559"],
+	info : {
+		title : "【閱樂書沙龍】",
+		content : [
+			"「一本書真實的價值，要看二手市場的排行榜。一張照片真實的重量，通常在多年後才知道。」喜歡在舊書店尋寶，做設計也寫書的Hally Chen，將從自己手上珍藏中，挑選幾本有趣的舊雜誌和舊書，分享這些刊物超越時間的價值和創意，探索另一種閱讀的收穫。"
+		],
+		date : "6/10(五) 19:30-21:00",
+		social : {
+			facebook : "",
+			google_plus : "",
+			twitter : "",
+			weibo : "",
+			fanily : "",
+		}
+	},
+	cast : [
+		{
+			"title" : "策展人 設計師",
+			"name" : "方序中",
+			"avatars" : "images/guests-1.jpg"
+		},
+		{
+			"title" : "設計師、作家",
+			"name" : "Hally Chen",
+			"avatars" : "images/guests-2.jpg?v=1"
+		}
+	],
+	login_title : [
+		"直播已經開始，趕快加入聊天室。",
+		"今夜，我們要從舊雜誌與舊書中，探索超越時間的價值和創意，趕快加入我們吧！"
+	],
+	article : {
+		title : '舊雜誌與舊書有什麼值得探索的奧秘？',
+		content : [
+			"閱",
+			"眼見文字",
+			"心裡會跟著念",
+			"這就是一種魔法",
+			"",
+			"樂",
+			"獲得能量",
+			"凝聚並分享",
+			"",
+			"透過創作者不同風格及閱讀方式",
+			"分享關於他們的閱樂"
+		]
+	}
+}
