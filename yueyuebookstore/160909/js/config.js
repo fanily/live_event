cebaf18338fce32673facc54766c67d8f31a36ec
JoeAngel <joe10330@gmail.com>
2016-09-05 21:12:48 +0800

    Add yueyuebookstore-160909

---
+++ yueyuebookstore/160909/js/config.js
@@ -0,0 +1,60 @@
+var config = {
+	start_time : "2016-09-09 19:25:00",
+	end_time : "2016-09-09 21:00:00",
+	close_time : "2016-09-09 21:30:00",
+	fb_app_id : "626114784069979",
+	live : {
+		id : "bash_history",
+		start_time : "",
+		playlist : "",
+	},
+	api : "https://live-api.fanily.tw",
+	api_socket : "http://live-api.fanily.tw",
+	moderator_uid : ["195841904090221","543628245","1714281551","100011616072642","126943094369559"],
+	info : {
+		title : "【閱樂書沙龍】",
+		content : [
+			"紙本與空間的微妙關係，像是平面到立體的編輯過程，面對舊空間與舊建物再使用的癥結，透過編輯的動作，傳達對空間使用的堅持與城市事件的紀錄，傳遞編輯的幾何生活。",
+		],
+		date : "9/09(五) 19:30-21:00",
+		social : {
+			facebook : "",
+			google_plus : "",
+			twitter : "",
+			weibo : "",
+			fanily : "",
+		}
+	},
+	cast : [
+		{
+			"title" : "策展人 設計師",
+			"name" : "方序中",
+			"avatars" : "images/guests-1.jpg"
+		},
+		{
+			"title" : "講師 雜誌編輯",
+			"name" : "佔空間 AJ",
+			"avatars" : "images/guests-2.jpg?v=1"
+		}
+	],
+	login_title : [
+		"直播已經開始，趕快加入聊天室。",
+		"這一晚將由佔空間的 AJ 與您分享，突破紙本到城市空間的編輯力。"
+	],
+	article : {
+		title : '透過編輯的另類眼光，洞悉舊空間的新可能，讓城市靈魂重版出來！',
+		content : [
+			"閱",
+			"眼見文字",
+			"心裡會跟著念",
+			"這就是一種魔法",
+			"",
+			"樂",
+			"獲得能量",
+			"凝聚並分享",
+			"",
+			"透過創作者不同風格及閱讀方式",
+			"分享關於他們的閱樂"
+		]
+	}
+}
