838a1c020528a7f5900b7bc647804cc473870bc9
JoeAngel <joe10330@gmail.com>
2016-09-13 15:19:47 +0800

    Add yueyuebookstore-160916

---
+++ yueyuebookstore/160916/js/config.js
@@ -0,0 +1,47 @@
+var config = {
+	start_time : "2016-09-16 19:25:00",
+	end_time : "2016-09-16 21:00:00",
+	close_time : "2016-09-16 21:30:00",
+	fb_app_id : "626114784069979",
+	live : {
+		id : "CmpTRuqqrOk",
+		start_time : "",
+		playlist : "",
+	},
+	api : "https://live-api.fanily.tw",
+	api_socket : "http://live-api.fanily.tw",
+	moderator_uid : ["195841904090221","543628245","1714281551","100011616072642","126943094369559"],
+	info : {
+		title : "【閱樂書沙龍】",
+		content : [
+			"「女爵・騎士」將結合精湛的現場演出，介紹經典爵士樂的特色與入門知識，讓觀眾在感受其豐富風格變化與即興表演的魅力之同時，建立品味爵士樂的基本方向與入門法則。聆聽經驗將充滿不可預期的驚喜，歡迎舊雨新手一起來搖擺！"
+		],
+		date : "9/16(五) 19:30-21:00",
+		social : {
+			facebook : "",
+			google_plus : "",
+			twitter : "",
+			weibo : "",
+			fanily : "",
+		}
+	},
+	cast : [
+		{
+			"title" : "演出樂團",
+			"name" : "女爵・騎士",
+			"avatars" : "images/guests-2.jpg?v=1"
+		}
+	],
+	login_title : [
+		"直播已經開始，趕快加入聊天室。",
+		"這一晚將由女爵・騎士為您呈現爵士樂的獨特風情。"
+	],
+	article : {
+		title : '一場書店裡的音樂會，同時也是輕鬆愉快的爵士閱賞析入門課！',
+		content : [
+			"週末夜晚必備好音樂！",
+			"透過迷人紓壓的爵士樂洗禮，",
+			"引領大家踏入聽覺美學殿堂。"
+		]
+	}
+}
